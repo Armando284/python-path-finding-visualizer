7d8b28118a59bc00cbadf48392939f2d132e48ef
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-05-31 00:23:43 -0400

    Adding first algorithm

--- pathfinding.py
+++ pathfinding.py
@@ -66,7 +66,7 @@ class Node:
     def make_end(self):
         self.color = TURQUOISE
 
-    def male_path(self):
+    def make_path(self):
         self.color = PURPLE
 
     def draw(self, win):
@@ -107,6 +107,60 @@ def make_grid(rows, width):
             node = Node(i, j, gap, rows)
             grid[i].append(node)
     return grid
+
+
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            # make path
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + \
+                    h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
 
 
 def draw_grid(win, rows, width):
@@ -181,7 +235,17 @@ def main(win, width):
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and not started:
-                    pass
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid)
+
+                    algorithm(lambda: draw(win, grid, ROWS, width),
+                              grid, start, end)
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
+
     pygame.quit()
 
 
